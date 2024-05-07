9bb79f33863a0be64be14ff8729177e57c7c9315
TTaannaakkaa <ee203081@meiji.ac.jp>
2024-05-07 16:57:43 +0900

    delete build

--- brushee_glocal_path_planner/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/amsl/brushee_ws/src/brushee_glocal_path_planner/brushee_glocal_path_planner/include".split(';') if "/home/amsl/brushee_ws/src/brushee_glocal_path_planner/brushee_glocal_path_planner/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lbrushee_glocal_path_planner".split(';') if "-lbrushee_glocal_path_planner" != "" else []
-PROJECT_NAME = "brushee_glocal_path_planner"
-PROJECT_SPACE_DIR = "/home/amsl/brushee_ws/src/brushee_glocal_path_planner/brushee_glocal_path_planner/build/devel"
-PROJECT_VERSION = "0.0.0"
